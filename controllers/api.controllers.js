7c56b56e9393ee3993ac24bb5ddef35b45afbc2d
asimditta <asimditta15@gmail.com>
2022-01-20 17:33:40 +0000

    refactor api endpoint

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 
 exports.getApi = (req, res) => {
   return fs.readFile("endpoints.json", "utf-8", (err, contents) => {
-    return res.status(200).send({ contents });
+    return res.status(200).send(JSON.parse({ contents }));
   });
 };
